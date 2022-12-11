584403967872174b9a9d2aa56f15f40775ff3837
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-11 12:43:26 +0100

    feat: add radix alert modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as AlertDialog from '@radix-ui/react-alert-dialog'
 
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+        <AlertDialog.Root>
+          <AlertDialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </AlertDialog.Trigger>
+
+          <NewTransactionModal />
+        </AlertDialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
