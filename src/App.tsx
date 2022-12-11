5072013f4c848e1590a5780250bc4dce4d6e2301
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-11 10:21:46 +0100

    feat: add styled-components theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+/**
+ * Usar styled-components
+ * -> npm i styled-components
+ * -> npm i @types/styled-components -D
+ *
+ * Folders (src/):
+ * -> styles
+ *    -> themes -> default.ts
+ *    -> global.ts
+ */
+
 export default function App() {
-  return <h1>hello world</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>hello world</h1>
+    </ThemeProvider>
+  )
 }
