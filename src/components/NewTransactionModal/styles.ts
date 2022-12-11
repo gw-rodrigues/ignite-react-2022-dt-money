233f0ba185cd6711a6da8a87ac7843043d05c7f7
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-11 12:43:56 +0100

    styles: alert modal form with radix componentes

---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+import * as AlertDialog from '@radix-ui/react-alert-dialog'
+
+export const Overlay = styled(AlertDialog.Overlay)`
+  position: fixed;
+  width: 100vw;
+  height: 110vh;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.75);
+`
+
+export const Content = styled(AlertDialog.Content)`
+  min-width: 32rem;
+  border-radius: 6px;
+  padding: 2.5rem 3rem;
+  background: ${(props) => props.theme['gray-800']};
+
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  form {
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    input {
+      border-radius: 6px;
+      border: 0;
+      background: ${(props) => props.theme['gray-900']};
+      color: ${(props) => props.theme['gray-300']};
+      padding: 1rem;
+
+      &::placeholder {
+        color: ${(props) => props.theme['gray-500']};
+      }
+    }
+
+    button {
+      height: 58px;
+      border: 0;
+      background: ${(props) => props.theme['green-500']};
+      color: ${(props) => props.theme.white};
+      font-weight: bold;
+      padding: 0 1.25rem;
+      border-radius: 6px;
+      margin-top: 1.5rem;
+      cursor: pointer;
+
+      &:hover {
+        background: ${(props) => props.theme['green-700']};
+        transition: background-color 0.2s;
+      }
+    }
+  }
+`
+
+export const CloseButton = styled(AlertDialog.Cancel)`
+  position: absolute;
+  background: transparent;
+  border: 0;
+  top: 1.5rem;
+  right: 1.5rem;
+  font-size: 0; //line-height:0; OU/ dá mesmo funcionamento
+  cursor: pointer;
+  color: ${(props) => props.theme['gray-500']};
+`
