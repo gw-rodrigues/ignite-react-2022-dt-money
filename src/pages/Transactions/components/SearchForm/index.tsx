0d55eaeec325d7deef7ed721eb52518934ea3b5f
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-12 16:47:17 +0100

    feat: add fetchTransitions to context

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,7 +1,9 @@
+import { useContext } from 'react'
 import { MagnifyingGlass } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 import { SearchFormContainer } from './styles'
 
 const searchFormSchema = z.object({
@@ -11,6 +13,8 @@ const searchFormSchema = z.object({
 type TSearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
+
   const {
     register,
     handleSubmit,
@@ -18,8 +22,7 @@ export function SearchForm() {
   } = useForm<TSearchFormInputs>({ resolver: zodResolver(searchFormSchema) })
 
   async function handleSearchTransactions(data: TSearchFormInputs) {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    console.log(data)
+    await fetchTransactions(data.query)
   }
 
   return (
