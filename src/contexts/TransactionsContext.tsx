6dbca7eb55e36a14dfe05ab13604d650815a24de
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-12 16:46:39 +0100

    feat: add context and search  method

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -11,28 +11,35 @@ interface ITransactions {
 
 interface ITransactionsContextTypes {
   transactions: ITransactions[]
+  fetchTransactions: (query?: string) => Promise<void>
 }
 
 interface ITransactionsProviderProps {
   children: ReactNode
 }
-export const TransactionsContext = createContext({} as ITransactionsContextTypes)
+export const TransactionsContext = createContext(
+  {} as ITransactionsContextTypes,
+)
 
 export function TransactionsProvider({ children }: ITransactionsProviderProps) {
   const [transactions, setTransactions] = useState<ITransactions[]>([])
 
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions')
+  async function fetchTransactions(query?: string) {
+    const url = new URL('http://localhost:3333/transactions')
+    if (query) {
+      url.searchParams.append('q', query)
+    }
+    const response = await fetch(url)
     const data = await response.json()
     setTransactions(data)
   }
 
   useEffect(() => {
-    loadTransactions()
+    fetchTransactions()
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   )
